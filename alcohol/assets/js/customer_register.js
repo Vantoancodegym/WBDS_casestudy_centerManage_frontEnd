d94e1d3b72687ed387c0f7b9255a844205b3d430
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-03-25 23:46:36 +0700

    thêm tính năng xem danh sách khách hàng đăng ký tư cấn

---
+++ alcohol/assets/js/customer_register.js
@@ -0,0 +1,31 @@
+function firstLoad(){
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        url: "http://localhost:8080/admin/findAllCustomerRegister",
+        type: 'GET',
+        error: function (err) {
+            alert("lỗi")
+        },
+        success: function (data) {
+            let content=`   <tr>
+            <th>Tên khách hàng</th>
+            <th>Số điện thoại</th>
+            <th>Email</th>
+            <th>Địa chỉ</th>
+            <th width="230">Nội dung</th>
+        </tr>`;
+            for (let i = 0; i < data.length; i++) {
+                content+=`<tr>
+<td>${data[i].customerName}</td>
+<td>${data[i].customerPhone}</td>
+<td>${data[i].email}</td>
+<td>${data[i].customerAddress}</td>
+<td>${data[i].content}</td>
+                         </tr>`
+            }
+            document.getElementById("listCustomer").innerHTML=content;
+        }});
+}
