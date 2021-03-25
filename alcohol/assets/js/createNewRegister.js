800534ab9cff3c7e005ac5361b27fe81db3f91b1
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-03-26 00:20:03 +0700

    thêm tính năng đăng ký tư vấn

---
+++ alcohol/assets/js/createNewRegister.js
@@ -0,0 +1,33 @@
+function createNewRegister(){
+    let name=$('#name').val();
+    let email=$('#email').val();
+    let phone=$('#phone').val();
+    let address=$('#address').val();
+    let content=$('#content').val();
+    let newRegister={
+        customerName:name,
+        email:email,
+        customerPhone:phone,
+        customerAddress:address,
+        content:content
+    }
+
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        url: "http://localhost:8080/admin/insertCustomerRegister",
+        type: 'POST',
+        data: JSON.stringify(newRegister),
+
+        error: function (err) {
+            alert("lỗi")
+        },
+        success: function (data) {
+            alert("Tạo mới thành công")
+            console.log(data)
+        }
+    });
+
+}
